54267ef6ec2388940be71428fd52629e805ee53c
Peter F Bradshaw <pfb@exadios.com>
2019-06-26 12:11:10 +0800

    GNURadio: Updated Test Receiver
    Changed center freq from ACMA documentation.
    
    Changed schema to be more realistic.

--- src/GNURadio/PWR_test_receiver.py
+++ src/GNURadio/PWR_test_receiver.py
@@ -5,7 +5,7 @@
 # Title: Passive Weather Radar test receiver
 # Author: Peter F Bradshaw
 # Description: This is a test receiver for the of the Passive Weather Radar
-# Generated: Tue Jun 25 10:38:53 2019
+# Generated: Wed Jun 26 12:08:17 2019
 ##################################################
 
 if __name__ == '__main__':
@@ -67,61 +67,11 @@ class PWR_test_receiver(gr.top_block, Qt.QWidget):
         ##################################################
         # Blocks
         ##################################################
-        self.test_display_1 = qtgui.time_sink_c(
-        	1024, #size
-        	samp_rate, #samp_rate
-        	"", #name
-        	1 #number of inputs
-        )
-        self.test_display_1.set_update_time(0.10)
-        self.test_display_1.set_y_axis(-1, 1)
-        
-        self.test_display_1.set_y_label('Correlation', "")
-        
-        self.test_display_1.enable_tags(-1, True)
-        self.test_display_1.set_trigger_mode(qtgui.TRIG_MODE_FREE, qtgui.TRIG_SLOPE_POS, 0.0, 0, 0, "")
-        self.test_display_1.enable_autoscale(False)
-        self.test_display_1.enable_grid(False)
-        self.test_display_1.enable_axis_labels(True)
-        self.test_display_1.enable_control_panel(False)
-        
-        if not True:
-          self.test_display_1.disable_legend()
-        
-        labels = ['', '', '', '', '',
-                  '', '', '', '', '']
-        widths = [1, 1, 1, 1, 1,
-                  1, 1, 1, 1, 1]
-        colors = ["blue", "red", "green", "black", "cyan",
-                  "magenta", "yellow", "dark red", "dark green", "blue"]
-        styles = [1, 1, 1, 1, 1,
-                  1, 1, 1, 1, 1]
-        markers = [-1, -1, -1, -1, -1,
-                   -1, -1, -1, -1, -1]
-        alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
-                  1.0, 1.0, 1.0, 1.0, 1.0]
-        
-        for i in xrange(2*1):
-            if len(labels[i]) == 0:
-                if(i % 2 == 0):
-                    self.test_display_1.set_line_label(i, "Re{{Data {0}}}".format(i/2))
-                else:
-                    self.test_display_1.set_line_label(i, "Im{{Data {0}}}".format(i/2))
-            else:
-                self.test_display_1.set_line_label(i, labels[i])
-            self.test_display_1.set_line_width(i, widths[i])
-            self.test_display_1.set_line_color(i, colors[i])
-            self.test_display_1.set_line_style(i, styles[i])
-            self.test_display_1.set_line_marker(i, markers[i])
-            self.test_display_1.set_line_alpha(i, alphas[i])
-        
-        self._test_display_1_win = sip.wrapinstance(self.test_display_1.pyqwidget(), Qt.QWidget)
-        self.top_layout.addWidget(self._test_display_1_win)
         self.target_stream_to_vector = blocks.stream_to_vector(gr.sizeof_gr_complex*1, 1024)
-        self.target_fft = fft.fft_vcc(1024, True, (window.rectangular(1024)), True, 1)
+        self.target_fft = fft.fft_vcc(1024, False, (window.rectangular(1024)), True, 1)
         self.rtlsdr_target_source = osmosdr.source( args="numchan=" + str(1) + " " + '' )
         self.rtlsdr_target_source.set_sample_rate(samp_rate)
-        self.rtlsdr_target_source.set_center_freq(970e6, 0)
+        self.rtlsdr_target_source.set_center_freq(882.5e6, 0)
         self.rtlsdr_target_source.set_freq_corr(0, 0)
         self.rtlsdr_target_source.set_dc_offset_mode(0, 0)
         self.rtlsdr_target_source.set_iq_balance_mode(0, 0)
@@ -134,7 +84,7 @@ class PWR_test_receiver(gr.top_block, Qt.QWidget):
           
         self.rtlsdr_reference_source = osmosdr.source( args="numchan=" + str(1) + " " + '' )
         self.rtlsdr_reference_source.set_sample_rate(samp_rate)
-        self.rtlsdr_reference_source.set_center_freq(970e6, 0)
+        self.rtlsdr_reference_source.set_center_freq(882.5e6, 0)
         self.rtlsdr_reference_source.set_freq_corr(0, 0)
         self.rtlsdr_reference_source.set_dc_offset_mode(0, 0)
         self.rtlsdr_reference_source.set_iq_balance_mode(0, 0)
@@ -146,17 +96,34 @@ class PWR_test_receiver(gr.top_block, Qt.QWidget):
         self.rtlsdr_reference_source.set_bandwidth(0, 0)
           
         self.refernce_stream_to_vector = blocks.stream_to_vector(gr.sizeof_gr_complex*1, 1024)
-        self.reference_fft = fft.fft_vcc(1024, True, (window.rectangular(1024)), True, 1)
+        self.reference_fft = fft.fft_vcc(1024, False, (window.rectangular(1024)), True, 1)
+        self.qtgui_sink_x_0 = qtgui.sink_c(
+        	1024, #fftsize
+        	firdes.WIN_BLACKMAN_hARRIS, #wintype
+        	0, #fc
+        	samp_rate, #bw
+        	"", #name
+        	True, #plotfreq
+        	True, #plotwaterfall
+        	True, #plottime
+        	True, #plotconst
+        )
+        self.qtgui_sink_x_0.set_update_time(1.0/10)
+        self._qtgui_sink_x_0_win = sip.wrapinstance(self.qtgui_sink_x_0.pyqwidget(), Qt.QWidget)
+        self.top_layout.addWidget(self._qtgui_sink_x_0_win)
+        
+        self.qtgui_sink_x_0.enable_rf_freq(False)
+        
+        
+          
         self.correlation_vector_to_stream = blocks.vector_to_stream(gr.sizeof_gr_complex*1, 1024)
-        self.correlation_fft = fft.fft_vcc(1024, False, (window.rectangular(1024)), True, 1)
         self.blocks_multiply_conjugate_cc_0 = blocks.multiply_conjugate_cc(1024)
 
         ##################################################
         # Connections
         ##################################################
-        self.connect((self.blocks_multiply_conjugate_cc_0, 0), (self.correlation_fft, 0))    
-        self.connect((self.correlation_fft, 0), (self.correlation_vector_to_stream, 0))    
-        self.connect((self.correlation_vector_to_stream, 0), (self.test_display_1, 0))    
+        self.connect((self.blocks_multiply_conjugate_cc_0, 0), (self.correlation_vector_to_stream, 0))    
+        self.connect((self.correlation_vector_to_stream, 0), (self.qtgui_sink_x_0, 0))    
         self.connect((self.reference_fft, 0), (self.blocks_multiply_conjugate_cc_0, 0))    
         self.connect((self.refernce_stream_to_vector, 0), (self.reference_fft, 0))    
         self.connect((self.rtlsdr_reference_source, 0), (self.refernce_stream_to_vector, 0))    
@@ -174,9 +141,9 @@ class PWR_test_receiver(gr.top_block, Qt.QWidget):
 
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
-        self.test_display_1.set_samp_rate(self.samp_rate)
         self.rtlsdr_target_source.set_sample_rate(self.samp_rate)
         self.rtlsdr_reference_source.set_sample_rate(self.samp_rate)
+        self.qtgui_sink_x_0.set_frequency_range(0, self.samp_rate)
 
 
 def main(top_block_cls=PWR_test_receiver, options=None):
