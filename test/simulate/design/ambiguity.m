ecd9dcdd9fb42725a8306f656e3d27ef7ab9bb5f
Peter F Bradshaw <pfb@exadios.com>
2019-08-13 23:02:59 +0800

    Test: Corrected ambiguity.m

--- test/simulate/design/ambiguity.m
+++ test/simulate/design/ambiguity.m
@@ -4,7 +4,7 @@ t_s = 4;
 t = [0: n_max - 1]'; z = randn(size(t));  % z is white Gaussian noise.
 tau = [-n_max: t_s: n_max]'; phi = [-0.16:0.016:16]';
 
-X = xamb(z, phi, 
+X = amb(z, t, tau, phi);
 mesh(10 * log10(X));
 disp('White noise');
 xlabel('\tau');
